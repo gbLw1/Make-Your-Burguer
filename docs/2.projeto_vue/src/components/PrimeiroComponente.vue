1d13f39f3d1b2608367ae00a0a86385032ceb59b
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-10-28 22:57:46 -0300

    #06 - Dados em componentes (data)

--- docs/2.projeto_vue/src/components/PrimeiroComponente.vue
+++ docs/2.projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,11 +1,20 @@
 <template>
-    <h1>Olá Vue!</h1>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
 </template>
 
 <script>
 
     export default {
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data() {
+            return {
+                nome: "Gabriel",
+                profissao: "Programador"
+            }
+        }
     }
 
 </script>

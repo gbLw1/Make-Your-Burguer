075da42a8525e34d226a5222afeb97e140a483aa
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-10-28 21:58:57 -0300

    #03 - Inputs e Data Binding

--- docs/1 - Instalação Vue/src/app.js
+++ docs/1 - Instalação Vue/src/app.js
@@ -2,7 +2,17 @@ const MyNameApp = {
     data() {
         return {
             name: "Gabriel",
-            age: 22
+            age: 22,
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+
+            console.log(this.input_name);
+
+            this.name = this.input_name;
         }
     }
 }

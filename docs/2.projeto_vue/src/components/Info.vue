e6baee9c505e5beac5b2b9fef31a0c4141b97c37
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-10-30 00:59:03 -0300

    #08 - Hierarquia de componentes

---
+++ docs/2.projeto_vue/src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>.NET C#</li>
+            <li>Python</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Info'
+}
+
+</script>
